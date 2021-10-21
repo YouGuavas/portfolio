b1a947ba45cd5b1024149ea69d4aa1612536db6a
Patrick Yambrick <patrickyambrick@gmail.com>
2021-10-21 12:43:41 -0400

    Make navbar sticky, fix bugs with active link toggling

--- src/App.js
+++ src/App.js
@@ -5,20 +5,27 @@ function App() {
   return (
     <div className="App">
       <Nav />
-      <div className="flex items-center justify-center h-screen">
-        <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
-          Hi, I'm Patrick 👋
-        </div>
-      </div>
       <Main />
+      <Work />
     </div>
   );
 }
 
 function Main() {
   return (
-    <div></div>
+    <div className="flex items-center justify-center h-screen">
+        <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
+          Hi, I'm Patrick 👋
+        </div>
+      </div>
   );
+}
+function Work() {
+  return(
+    <div id="Work">
+      Work
+    </div>
+  )
 }
 
 export default App;
--- src/Nav.js
+++ src/Nav.js
@@ -4,19 +4,55 @@ class Nav extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      activeIndex: 0
     };
   }
-  handleClick = () => {
-    const btn = document.getElementById("nav-button")
+  toggle = (menuType='', index=undefined) => {
+    let toggleClasses;
+    const oldIndex = this.state.activeIndex;
+    this.setState({activeIndex: index}, () => {
+      if (menuType === 'mobile') {
+        toggleClasses = ['text-white', 'hover:text-white', 'bg-green-500', 'hover:bg-green-500', 'font-semibold', 'transition', 'duration-300'];
+        const active = document.getElementById(`mobileMenu${oldIndex}`);
+        const activeA = document.querySelector(`#mobileMenu${oldIndex}>a`);
+        const inactive = document.getElementById(`mobileMenu${this.state.activeIndex}`);
+        const inactiveA = document.querySelector(`#mobileMenu${this.state.activeIndex}>a`);
+        active.classList.toggle('active');
+        inactive.classList.toggle('active');
+        toggleClasses.map((toggleClass) => {
+          activeA.classList.toggle(toggleClass);
+          inactiveA.classList.toggle(toggleClass);
+          return null;
+        });
+      } else {
+        toggleClasses = ['text-gray-500', 'text-green-500', 'hover:text-green-500', 'border-b-4', 'border-green-500', 'transition', 'duration-300'];
+        const active = document.getElementById(`bigMenu${oldIndex}`);
+        const inactive = document.getElementById(`bigMenu${this.state.activeIndex}`)
+        toggleClasses.map((toggleClass) => {
+          active.classList.toggle(toggleClass);
+          inactive.classList.toggle(toggleClass);
+          return null;
+        });
+      }
+    })
+    
+  }
+  handleClick = (menuType='', item='', index=undefined) => {
+    const btn = document.getElementById("nav-button");
     const menu = document.getElementById("mobile-menu");
-    menu.classList.toggle('hidden');
-    btn.classList.toggle('hidden');
+    if (menuType === 'mobile') {
+      menu.classList.toggle('hidden');
+      btn.classList.toggle('hidden');
+    } 
+    if (item.length > 0) {
+      this.toggle(menuType, index);
+    }
+    
   }
   render() {
-  const links = ['Home', 'About', 'Contact Me'];
+  const links = ['Home', 'Work', 'Contact Me'];
   return(
-    <nav className='bg-white shadow-lg'>
+    <nav className='sticky top-0 z-50 bg-white shadow-lg'>
       <div className='max-w-6xl mx-auto px-4'>
         <div className='flex justify-between'>
           <div className='flex space-x-7'>
@@ -28,17 +64,13 @@ class Nav extends React.Component {
               </a>
             </div>
 					<div className="hidden md:flex items-center space-x-1">
-            {links.map((item) => (
-               <a href="" className="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300">{item}</a>
+            {links.map((item, index) => (
+               index === 0 ? <a href='#' key={index} id={`bigMenu${index}`} className="py-4 px-2 text-green-500 border-b-4 border-green-500 font-semibold" onClick={() => {this.handleClick('', item, index)}}>{item}</a> : <a href={`#${item}`} key={index} id={`bigMenu${index}`} className="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300" onClick={() => {this.handleClick('', item, index)}}>{item}</a>
             ))}
-						{/*<a href=""
-							className="py-4 px-2 text-green-500 border-b-4 border-green-500 font-semibold ">
-                Home
-            </a>*/}
             </div>
           </div>
           <div className="md:hidden flex items-center">
-            <button id="nav-button" className="outline-none mobile-menu-button" onClick={this.handleClick}>
+            <button id="nav-button" className="outline-none mobile-menu-button" onClick={() => {this.handleClick("mobile")}}>
               <svg className="w-6 h-6 text-gray-500"
                 x-show="!showMenu"
                 fill="none"
@@ -53,8 +85,8 @@ class Nav extends React.Component {
         </div>
         <div id="mobile-menu" className="hidden mobile-menu">
           <ul className="">
-            {links.map((item) => (
-              <li><a href={`#${item}`} className="block text-sm px-2 py-4 hover:bg-green-500 transition duration-300">{item}</a></li>
+            {links.map((item, index) => (
+              index === 0 ? <li className="active" key={index} id={`mobileMenu${index}`}><a href="#" className="block text-sm px-2 py-4 text-white bg-green-500 font-semibold" onClick={() => {this.handleClick("mobile", item, index)}}>{item}</a></li> : <li key={index} id={`mobileMenu${index}`}><a href={`#${item}`} className="block text-sm px-2 py-4 hover:bg-green-500 hover:text-white transition duration-300" onClick={() => {this.handleClick("mobile", item, index)}}>{item}</a></li>
             ))}
             {/*
             <li className="active"><a href="index.html" className="block text-sm px-2 py-4 text-white bg-green-500 font-semibold">Home</a></li>
