e745599fc637c79f42fd7d34ecb434571416b9e7
Patrick Yambrick <patrickyambrick@gmail.com>
2022-01-05 19:07:14 -0500

    Add padding

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <div className="h-screen bg-green-500 grid md:grid-cols-3 grid-cols-4 grid-rows-3">
+      <div className="p-6 h-screen bg-green-500 grid md:grid-cols-3 grid-cols-4 grid-rows-3">
         <div className="row-start-2 row-span-1 md:col-start-2 md:col-span-1 col-start-1 col-span-4 grid place-items-center text-center bg-black text-white rounded-lg border-2 border-white shadow-lg">
           <p className="font-bold ">Hi, I'm Patrick 👋</p>
           <p>This website was made with: React & Tailwind</p>
