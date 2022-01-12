291d971129ebdc795891c016ecd4ac1e74bd7e5d
Patrick Yambrick <patrickyambrick@gmail.com>
2022-01-12 16:35:54 -0500

    Improve link safety with noopener

--- src/Work.js
+++ src/Work.js
@@ -11,7 +11,7 @@ export default function Work() {
   return (
     <div className="h-screen bg-green-500 p-6 text-center">
       <section className="text-white">
-        <h2 className="font-bold">Personal Projects</h2>
+        <p className="font-bold">Personal Projects</p>
         <p>These are projects I have done to further my own learning, comprehension, and growth.</p>
         <div className="grid grid-cols-3 py-6">
           {Object.keys(projects).map((item) => { 
@@ -28,11 +28,13 @@ export default function Work() {
 function Card(props) {
   return(
   <div className="md:col-span-1 col-span-3 p6 h-100 bg-green-700 border-2 border-black rounded grid grid-cols-2 place-items-center text-center">
-    <h6 className="col-span-2 py-2">{props.project.title}</h6>
-    <img className="col-span-2 py-2" alt="" src={props.project.src} />
+    <p className="col-span-2 py-2">{props.project.title}</p>
+    <div className="col-span-2 p-2 bg-white">
+      <img alt="Screenshot of project page" src={props.project.src} />
+    </div>
     <p className="col-span-2 py-2">{props.project.description}</p>
-    <a href={props.project.live} target="_blank" className="col-span-2 w-1/2 hover:bg-green-800 border rounded p-2 my-2 hover:text-gray-300 font-semibold">View it live</a>
-    <a href={props.project.github} target="_blank" className="col-span-2 w-1/2 border rounded p-2 my-2 hover:text-gray-300 hover:bg-green-800 font-semibold">View it on github</a>
+    <a href={props.project.live} target="_blank" rel="noopener" className="col-span-2 w-1/2 hover:bg-green-800 border rounded p-2 my-2 hover:text-gray-300 font-semibold">View it live</a>
+    <a href={props.project.github} target="_blank" rel="noopener" className="col-span-2 w-1/2 border rounded p-2 my-2 hover:text-gray-300 hover:bg-green-800 font-semibold">View it on github</a>
   </div>
   )
 }
