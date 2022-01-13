c835efffdbdd5eafdd3ad0ba626803bfe85c15cf
Patrick Yambrick <patrickyambrick@gmail.com>
2022-01-12 19:48:50 -0500

    Improve contrast

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,11 @@
 export default function Home() {
   return (
     <div>
-      <div className="p-6 h-screen bg-green-500 grid md:grid-cols-3 grid-cols-4 grid-rows-3">
-        <div className="font-body p-5 row-start-2 row-span-1 md:col-start-2 md:col-span-1 col-start-1 col-span-4 grid place-items-center text-center bg-black text-white rounded-lg border-2 border-white shadow-lg">
-          <p className="font-bold">Hi, I'm Patrick 👋</p>
+      <div className="p-6 h-screen bg-green-900 grid md:grid-cols-3 grid-cols-4 grid-rows-3">
+        <div className="font-body p-5 row-start-2 row-span-1 md:col-start-2 md:col-span-1 
+        col-start-1 col-span-4 grid place-items-center text-center 
+        bg-gray-200 text-black rounded-lg border-2 border-black shadow-lg">
+          <p className="">Hi, I'm Patrick 👋</p>
           <p>This website was made with: React & Tailwind</p>
         </div>
       </div>
--- src/Work.js
+++ src/Work.js
@@ -9,8 +9,8 @@ export default function Work() {
     }
   }
   return (
-    <div className="h-screen bg-green-500 p-6 text-center">
-      <section className="text-white">
+    <div className="h-screen bg-green-900 p-6 text-center">
+      <section className="text-white font-body">
         <p className="font-bold">Personal Projects</p>
         <p>These are projects I have done to further my own learning, comprehension, and growth.</p>
         <div className="grid grid-cols-3 py-6">
@@ -27,14 +27,14 @@ export default function Work() {
 
 function Card(props) {
   return(
-  <div className="md:col-span-1 col-span-3 p6 h-100 bg-green-700 border-2 border-black rounded grid grid-cols-2 place-items-center text-center">
+  <div className="md:col-span-1 col-span-3 p-6 h-100 bg-green-800 border-2 border-black rounded grid grid-cols-2 place-items-center text-center">
     <p className="col-span-2 py-2">{props.project.title}</p>
     <div className="col-span-2 p-2 bg-white">
       <img height="134" width="250" alt="Screenshot of project page" src={props.project.src} />
     </div>
     <p className="col-span-2 py-2">{props.project.description}</p>
-    <a href={props.project.live} target="_blank" rel="noopener" className="col-span-2 w-1/2 hover:bg-green-800 border rounded p-2 my-2 hover:text-gray-300 font-semibold">View it live</a>
-    <a href={props.project.github} target="_blank" rel="noopener" className="col-span-2 w-1/2 border rounded p-2 my-2 hover:text-gray-300 hover:bg-green-800 font-semibold">View it on github</a>
+    <a href={props.project.live} target="_blank" rel="noopener" className="col-span-2 font-semibold w-full hover:bg-green-800 border rounded p-2 my-2 hover:text-gray-300">View it live</a>
+    <a href={props.project.github} target="_blank" rel="noopener" className="col-span-2 w-full font-semibold border rounded p-2 my-2 hover:text-gray-300 hover:bg-green-800">View it on github</a>
   </div>
   )
 }
