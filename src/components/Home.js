066b11737d8e3c1fb9fdff938994a53caa1b8a12
Patrick Yambrick <patrickyambrick@gmail.com>
2022-03-19 21:48:31 -0400

    Add About component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,38 @@
 export default function Home() {
   return (
     <div>
-      <div className="p-6 h-screen my-forest-bg grid md:grid-cols-3 grid-cols-4 grid-rows-3">
+      <section className="p-6 h-screen my-forest-bg grid md:grid-cols-3 grid-cols-4 grid-rows-3">
         <div className="font-body p-5 row-start-2 row-span-1 md:col-start-2 md:col-span-1 
         col-start-1 col-span-4 animate-ping-3 grid place-items-center text-center 
         my-sage-bg my-text-forest rounded-lg border-2 border-mustard shadow-lg">
           <p className="my-text-forest">Hi, I'm Patrick 👋</p>
           <p className="my-text-forest">I'm a full-stack developer, and I love learning new things.</p>
         </div>
-      </div>
+      </section>
       <div className="h-screen my-sage-bg">
+        <About />
         <Skills />
       </div>
-      </div>
+    </div>
   );
 }
+
+function About() {
+  return (
+    <section className="my-text-forest pt-6 px-6">
+      <h3 className="font-bold text-center">About Me</h3>
+      <p className="px-12">My name is Patrick! Welcome to my site! 
+        Here you will learn a bit about me as a person, 
+        and about the things that I enjoy doing. 
+        I like to create things: websites, woodworking projects, 
+        and a bit of art here and there. 
+        <a className="font-bold my-mustard-hover rounded" href="/contact%20me"> Contact me </a> 
+        if you think you'd like me to create something for you. 
+        Feel free to tell me a bit about it, and about yourself!</p>
+    </section>
+  )
+}
+
 
 function Skills() {
   const skills = [{name: 'React', url: 'https://reactjs.org/'}, 
@@ -23,16 +41,16 @@ function Skills() {
     {name: 'Bootstrap', url: ''}, {name: 'Bulma', url: ''}
   ];
   return(
-    <div className="my-text-forest p-6">
+    <section className="my-text-forest pt-6 px-12">
       <h3 className="font-bold text-center">Skills</h3>
       <ul className="grid grid-cols-2 gap-2 p-6 text-center border-2 border-forest md:mt-6 mt-2">
         {skills.map((item, index) => {
           return (
-            <li key={index} className="md:col-span-1 col-span-2"><a className="p-2 font-semibold my-mustard-hover" href={item.url}>{item.name}</a></li>
+            <li key={index} className="md:col-span-1 col-span-2"><a className="p-2 font-semibold my-mustard-hover rounded" href={item.url}>{item.name}</a></li>
           )
         })}
         
       </ul>
-    </div>
+    </section>
   )
 }
