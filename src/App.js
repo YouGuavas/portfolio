ed2fbad8ea490c46b7dd2df5ce59e27eaaabdacb
Patrick <patrickyambrick@Donnovans-MacBook-Pro.local>
2022-06-29 16:23:26 -0400

    Add Blog and Store components

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from './components/Home';
 import Work from './components/Work';
 import CurrentObsession from './components/CurrentObsession';
 import Contact from './components/Contact';
+import { Blogs } from './components/Blog';
 
 function App() {
   return (
@@ -13,13 +14,14 @@ function App() {
       <a href="/">
           <img alt="My personal logo" className="my-logo" height="96" width="96" src="https://lh3.googleusercontent.com/pw/AM-JKLXobqMgXDoXyzPRKm742CJiAwM_7Jq7miVxfm5-L_3tXV6xpDGPtClU_Tj8auUFChiXbqCnndNoa6b4t-ivMEin6PTsAuU8IdDoeP8WlagAdMvDELigoAhvSeCS2kSyY0aNUT1PaeD3VM7We2tX58bq=s96-no?authuser=0" />
         </a>
-      <Nav links={['Home', 'Work', 'Current Obsession', 'Contact Me']}/>
+      <Nav links={['Home', 'Work', 'Current Obsession', 'Blog', 'Contact Me']}/>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />}/>
           <Route path="/home" element={<Home />}/>
           <Route path="/work" element={<Work />}/>
           <Route path="/current%20obsession" element={<CurrentObsession />}/>
+          <Route path="/blog" element={<Blogs />}/>
           <Route path="/contact%20me" element={<Contact />}/>
         </Routes>
       </BrowserRouter>
---
+++ src/components/Blog.js
@@ -0,0 +1,14 @@
+import '../styles/Blog.scss';
+import '../styles/App.scss';
+
+
+
+export function Blogs() {
+    return (
+        <div className=" place-items-center w-full h-screen text-center my-text">
+            <section>
+             <h1 className="my-title">Blog Posts</h1>
+            </section>
+        </div>
+    )
+}
