e1862108ed3c28a87df480c37945057dd54a7c38
Patrick <patrickyambrick@Donnovans-MacBook-Pro.local>
2022-06-30 18:32:39 -0400

    Generate titles for Blog components

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,12 +1,17 @@
 import '../styles/Blog.scss';
 import '../styles/App.scss';
 import { Link, useParams } from 'react-router-dom';
+import { useEffect } from 'react';
 
 
 
 
 export function Blogs(props) {
     const posts = props.posts;
+    useEffect(() => {
+        document.title = "Blogs | Patrick Yambrick";
+       }, [document.title]
+     )
     return (
         <div className="p-6 place-items-center w-full h-screen text-center my-text">
             <section className="my-text font-body">
@@ -24,10 +29,15 @@ export function Blogs(props) {
 export function Blog(props) {
     const { BLOG_ID } = useParams();
     const posts = props.posts;
+    const title = posts[BLOG_ID].title;
+    useEffect(() => {
+        document.title = `${title} | Patrick Yambrick`;
+       }, [document.title]
+     )
     return (
         <div className="p-6 place-items-center w-full h-screen text-center my-text">
             <section className="my-text font-body">
-                <h1 className='my-title'>{posts[BLOG_ID].title}</h1>
+                <h1 className='my-title'>{title}</h1>
                 {posts[BLOG_ID].body}
             </section>
         </div>
