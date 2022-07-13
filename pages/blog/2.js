36286df0592851eff382fc9980245f25a0a16b7c
Patrick <patrickyambrick@gmail.com>
2022-07-13 12:57:50 -0400

    Blog updates

---
+++ pages/blog/2.js
@@ -0,0 +1,12 @@
+import BlogPost from '../../components/BlogPost';
+
+export default function () {
+    return (
+    <BlogPost title="Converting from CRA to Nextjs">
+        <h1 className="my-title">
+            Converting From Create-React-App to Nextjs
+        </h1>
+        This is my post
+    </BlogPost>
+    )
+}
