d292828cd68e529662d48c9d6fbd8d5356b58a85
Patrick Yambrick <patrickyambrick@gmail.com>
2023-02-16 13:14:41 -0500

    Add react-scroll to nav

--- components/Nav.js
+++ components/Nav.js
@@ -1,64 +1,38 @@
 import styles from '../styles/Nav.module.scss';
 
-import Link from 'next/link';
-import Image from 'next/image';
+import { Link } from 'react-scroll'
+
+
+//import Link from 'next/link';
 import {useState, useEffect} from 'react';
 
-import logo from '../public/images/logo.png';
 
 
 export default function Nav(props) {
   const links = props.links;
 
   const [active, setActive] = useState('');
-  const toggleMenu = () => {
-    const mobileMenu = document.getElementById('menu-full');
-    const mobileBtn = document.getElementById('menu-button-full');
-    mobileMenu.classList.toggle(styles.change);
-    mobileBtn.classList.toggle(styles.change);
-  }
+  
   const renderLinks = (item, index) => {
-    /*if (item == 'Home') {
-      return( 
-      <Link href="/" key={index}>
-          <li onClick={toggleMenu} className={(active === `/${item}`) || (active === `/${item.split(' ').join('')}`) || ((active === '/') && (item === 'Home')) ? `${styles.active} ${styles.standardMenuLink} ${styles.standardMenuItem}` : `${styles.standardMenuLink} ${styles.standardMenuItem}`}>
-            {item}
-          </li>
-      </Link>
-      )
-    } else {
-      return (
-        <Link href={`#${item}`} key={index}>
-          <li onClick={toggleMenu} className={(active === `/${item}`) || (active.indexOf('/'+item+'/') !== -1) || (active === `/${item.split(' ').join('%20')}`) || ((active === '/') && (item === 'Home')) ? `${styles.active} ${styles.standardMenuLink} ${styles.standardMenuItem}` : `${styles.standardMenuLink} ${styles.standardMenuItem}`}>
-            {item}
-          </li>
-        </Link>
-      )
-    }*/
+    
     return (
-      <Link name={`Nav ${item}`} href={`#${item}`} scroll={false} key={index}>
-        <li onClick={() => {setActive(`#${item}`)}} className={(active === `#${item}`) || (active.indexOf('/'+item+'/') !== -1) || (active === `/${item.split(' ').join('%20')}`) || ((active === '/') && (item === 'Home')) ? `${styles.active} ${styles.standardMenuLink} ${styles.standardMenuItem}` : `${styles.standardMenuLink} ${styles.standardMenuItem}`}>
+      <li>
+
+      <Link className={`${styles.standardMenuLink} ${styles.standardMenuItem}`} activeClass={styles.active} name={`Nav ${item}`} spy to={item} key={index}>
           {item}
-        </li>
       </Link>
+      </li>
     )
   }
 
   useEffect(function() {
-    //const activePage = window.location.pathname.split('#').join('');
     setActive(window.location.pathname);
   }, [links]);
  
   return (
     <header className={styles.navHeader}>
-      {/*<button aria-label="Full Menu Button" onClick={toggleMenu} id="menu-button-full" className={styles.menuButton}>
-              <div className={styles.bar1}></div>
-              <div className={styles.bar2}></div>
-              <div className={styles.bar3}></div>
-  </button>*/}
       <nav className={styles.myNavFull} id="menu-full">
         
-        {/*Standard menu, hides below laptop size*/}
         <ul className={styles.standardMenu}>
             {links.map((item, index) => {
                 return renderLinks(item, index)
