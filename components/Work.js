c379c5428799634c51b637d919ff02ec28448d74
Patrick Yambrick <patrickyambrick@gmail.com>
2023-02-27 10:24:14 -0500

    Add collectible shop project

--- components/Work.js
+++ components/Work.js
@@ -3,7 +3,7 @@ import codeEditor1 from '../public/images/codeEditor1.png';
 import codeEditor2 from '../public/images/codeEditor2.png';
 import portfolio1 from '../public/images/portfolio1.png';
 import portfolio2 from '../public/images/portfolio2.png';
-import showcase1 from '../public/images/showcase1.png';
+import shop1 from '../public/images/shop1.png';
 import showcase2 from '../public/images/showcase2.png';
 import unitConverter1 from '../public/images/unitConverter1.png';
 import unitConverter2 from '../public/images/unitConverter2.png';
@@ -16,20 +16,12 @@ export default function Work() {
   const projects = {
     codeEditor: {
       title: 'Code Editor',
-      description: 'A code editor I built using CodeMirror, react, and Sass. Users should be able to write html, css, and javascript code, and execute it in the iframe below. Html and Css update live, and javascript will run when the button is pressed.',
+      description: 'A code editor I built using CodeMirror, React, and Sass. Users should be able to write html, css, and javascript code, and execute it in the iframe below. Html and Css update live, and javascript will run when the button is pressed.',
       github: 'https://github.com/YouGuavas/code-editor',
       live: 'https://code-editor-vn5t7dek6-youguavas.vercel.app/',
       src1: codeEditor1.src,
       src2: codeEditor2.src
     },
-    /*gardenTracker: {
-      title: 'Garden Tracker',
-      description: 'This is a gardening tracker I built using the MERN stack and Sass. Users should be able to view general information about plants, register/login, and, if registered and logged in, add, and view data about their gardens.',
-      github: 'https://github.com/YouGuavas/gardening-tracker',
-      live: 'https://guavas-garden-tracker.herokuapp.com/',
-      src1: 'https://lh3.googleusercontent.com/pw/AM-JKLW-yAXZeV9XSZyly3w3sglULjrjOk3EWk6LVlK5UYiS9ceZxjmmIb-I2_Vi2IQ0Q073AOjiGyKuXMCmVBrOsH9sl1ZtAEFxo2IznBnbgRdYdR0jlPI76vTkltfZ5_-zNj1_nAl5QvEcqSJxAD6LEFpm=w1178-h604-no?authuser=0',
-      src2: 'https://lh3.googleusercontent.com/pw/AM-JKLW-yAXZeV9XSZyly3w3sglULjrjOk3EWk6LVlK5UYiS9ceZxjmmIb-I2_Vi2IQ0Q073AOjiGyKuXMCmVBrOsH9sl1ZtAEFxo2IznBnbgRdYdR0jlPI76vTkltfZ5_-zNj1_nAl5QvEcqSJxAD6LEFpm=w1178-h604-no?authuser=0',
-    },*/
     portfolio: {
       title: "This Portfolio Website",
       description: "A portfolio site I built to display my work using React & Tailwind.",
@@ -38,12 +30,12 @@ export default function Work() {
       src2: portfolio2.src
 
     },
-    showcase: {
-      title: "React Component Showcase",
-      description: "This is a showcase of components I have built with React and Sass.",
+    shop: {
+      title: "Collectible Shop",
+      description: "An ecommerce site for an imaginary collectible shop, built with React, Typescript, and Snipcart.",
       github: "https://github.com/YouGuavas/component-showcase",
       live: "https://guava-component-showcase.web.app/",
-      src1: showcase1.src,
+      src1: shop1.src,
       src2: showcase2.src,
 
     },
@@ -55,15 +47,6 @@ export default function Work() {
       src1: unitConverter1.src,
       src2: unitConverter2.src
     },
-    /*pokemon: {
-      title: "Pokemon Collection Tracker",
-      description: "This is a tool for keeping track of your Pokemon cards collection. Users should be able to add, remove, and view cards in their collection, as well as view cards yet to be collected. I built this project using the MERN stack and Sass.",
-      github: "https://github.com/YouGuavas/pokemon-tracker",
-      live: "https://pokemon-collection-tracker.herokuapp.com/",
-      src1: "https://lh3.googleusercontent.com/pw/AM-JKLVglu0wIYmnxolOpFBAy0T5Y4mw_nm-95AvDYCXM94Pr5vkTKu8DluYF1g0gB_agQEVy17i21LwUTS-65kke8ZXDyIWQH0UmNLKwpzK7MI_5RNjwgGY8zYQI0VT4pk5-ThVRl6wrkH_RN55GYdoBvid=w500-h285-no?authuser=0",
-      src2: "https://lh3.googleusercontent.com/pw/AM-JKLVglu0wIYmnxolOpFBAy0T5Y4mw_nm-95AvDYCXM94Pr5vkTKu8DluYF1g0gB_agQEVy17i21LwUTS-65kke8ZXDyIWQH0UmNLKwpzK7MI_5RNjwgGY8zYQI0VT4pk5-ThVRl6wrkH_RN55GYdoBvid=w500-h285-no?authuser=0"
-
-    },*/
   }
   return (
     <section id="Work" className={`${styles.work} h-full my-forest-bg p-6 text-center my-text-mustard font-body`}>
--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ import styles from '../styles/Home.module.scss';
 function index() {
   
   return (
-    <div>
+    <div >
         <Head>
             <title>Home | Patrick Yambrick</title>
         </Head>
