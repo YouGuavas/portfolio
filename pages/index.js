7487d32710c0490e5795142da5e34f1bae432664
Patrick Yambrick <patrickyambrick@gmail.com>
2023-03-02 14:45:54 -0500

    Update About

--- pages/index.js
+++ pages/index.js
@@ -50,13 +50,24 @@ function About() {
   return (
     <section id="About" className={`my-bg-sage px-6 vpwidth ${styles.about}`}>
       <h3 className="font-bold text-center my-title" id="About">About Me</h3>
-      <p className="px-12">My name is Patrick! Welcome to my site! 
-        Here you will learn a bit about me as a person, 
-        and about the things that I enjoy doing. 
-        I like to create things: websites, woodworking projects, 
-        and a bit of art here and there. 
+      <p className="px-12">Hi, my name is Patrick! Welcome to my site!</p>
+        <p className="px-12"> 
+        I enjoy creating things: websites, woodworking projects, 
+        and a bit of art here and there. I am most passionate about building things with code.
+        More specifically, I love to create things for the web. 
+        As a child, I always fantasized about building
+        robots and things, so you can imagine my joy in finally discovering and diving into
+        python just after high school. I dabbled in that for a while, then forgot about it
+        while I explored the world around me. Some years later, I returned to python, 
+        and from there began to consider coding as a career. That's how I discovered
+        HTML, CSS, and Javascript. I thought they'd be an easy way to get my feet wet 
+        in the world of programming for pay, and I never could have foreseen 
+        the joy and fun they would bring. Now I do my best to write some piece of code
+        for a web project every day, and I am always looking to learn new tricks
+        while doing so. I aspire to be the best developer I am capable of being!
+        </p>
+        <p className="px-12">
         I&apos;d like to help you build something, and <Link className="font-bold rounded my-fade-in my-dotted-underline" href="/Contact">I want you to tell me all about it</Link>.</p>
-      <p className="px-12">I&apos;m a full-stack developer, and I love learning new things.</p>
     </section>
   )
 }
