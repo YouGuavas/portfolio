747b6df96208fb73d4d1628f9895a542a891d6e3
Patrick Yambrick <patrickyambrick@gmail.com>
2023-03-15 12:35:54 -0400

    Add footer

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '../styles/globals.scss';
 import React from 'react';
 import Nav from '../components/Nav';
+import Footer from '../components/Footer';
 import Image from 'next/image';
 import Link from 'next/link';
 import logo from '../public/images/logo.png';
@@ -8,12 +9,14 @@ import logo from '../public/images/logo.png';
 function App({Component, pageProps}) {
   return (
     <React.Fragment>
-        <Link name="Logo Home" href="#Home" scroll={false}>
-          <Image alt="My personal logo" className="my-logo" height={96} width={96} src={logo.src} />
-        </Link>
-          <Nav links={['Home', 'About', 'Work', 'Skills', 'Contact']} pages={['about', 'work', 'contact']}/> {/*'Blog', 'Obsessions' */}
+      <Link name="Logo Home" href="#Home" scroll={false}>
+        <Image alt="My personal logo" className="my-logo" height={96} width={96} src={logo.src} />
+      </Link>
+      <Nav links={['Home', 'About', 'Work', 'Skills', 'Contact']} pages={['about', 'work', 'contact']}/> {/*'Blog', 'Obsessions' */}
 
       <Component {...pageProps} />
+
+      <Footer />
     </React.Fragment>
   );
 }
