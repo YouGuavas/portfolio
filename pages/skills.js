9794881e3a7f60b8e305e6460c8b49dc6a024395
Patrick Yambrick <patrickyambrick@gmail.com>
2023-03-15 12:30:38 -0400

    Make skills its own page

---
+++ pages/skills.js
@@ -0,0 +1,25 @@
+import styles from '../styles/Home.module.scss';
+
+export default function Skills() {
+    const skills = [{name: 'React', url: 'https://reactjs.org/'}, 
+      {name: 'Express', url: 'https://expressjs.com/'}, {name: 'MongoDB', url: 'https://www.mongodb.com/'},
+      {name: 'Sass', url: 'https://sass-lang.com/'}, {name: 'Tailwind', url: 'https://tailwindcss.com/'}, 
+      {name: 'Bootstrap', url: 'https://getbootstrap.com/'}, {name: 'Bulma', url: 'https://bulma.io/'},
+      {name: 'Nextjs', url: 'https://nextjs.org/'}, {name: 'Typescript', url: 'https://www.typescriptlang.org/'},
+  
+    ];
+    return(
+      <section id="Skills" className={`${styles.skills} my-text vpwidth vpheight`}>
+        <h3 className="text-center my-title padded-title">Skills</h3>
+        <ul className="grid rounded grid-cols-2 gap-2 p-6 text-center my-text md:mt-6 mt-2">
+          {skills.map((item) => {
+            return (
+              <a className="col-span-1 p-2" href={item.url} key={item.name}><li className={`my-text-forest-hover ${styles.listItem}`}>{item.name}</li></a>
+            )
+          })}
+          
+        </ul>
+      </section>
+    )
+  }
+  
