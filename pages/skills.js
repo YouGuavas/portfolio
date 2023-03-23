65e44f9b1d5facad1dd1325b45f8d10b750131c6
Patrick Yambrick <patrickyambrick@gmail.com>
2023-03-23 19:52:55 -0400

    Add descriptions to skills

--- pages/skills.js
+++ pages/skills.js
@@ -1,13 +1,8 @@
 import styles from '../styles/skills.module.scss';
 
-export default function Skills() {
-    const skills = [{ name: 'React', url: 'https://reactjs.org/' },
-    { name: 'Express', url: 'https://expressjs.com/' }, { name: 'MongoDB', url: 'https://www.mongodb.com/' },
-    { name: 'Sass', url: 'https://sass-lang.com/' }, { name: 'Tailwind', url: 'https://tailwindcss.com/' },
-    { name: 'Bootstrap', url: 'https://getbootstrap.com/' }, { name: 'Bulma', url: 'https://bulma.io/' },
-    { name: 'Nextjs', url: 'https://nextjs.org/' }, { name: 'Typescript', url: 'https://www.typescriptlang.org/' },
+import skills from '../data/skills.json';
 
-    ];
+export default function Skills() {
 
     return (
         <section id="Skills" className={`${styles.skills} my-background-generic my-text`}>
@@ -16,7 +11,13 @@ export default function Skills() {
                 <ul className="grid rounded grid-cols-2 gap-2 p-6 text-center my-text md:mt-6 mt-2">
                     {skills.map((item) => {
                         return (
-                            <a className="col-span-1 p-2" href={item.url} key={item.name}><li className={`my-text-forest-hover ${styles.listItem}`}>{item.name}</li></a>
+                            <a className="col-span-1 p-2" href={item.url} key={item.name}>
+                                <li className={`my-text-forest-hover ${styles.listItem}`}>
+                                    {item.name}
+                                    <caption>
+                                        {item.description ? item.description : null}
+                                    </caption>
+                                </li></a>
                         )
                     })}
 
