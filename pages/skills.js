6015d634f83601dbce9714fd5c38d5ae91a5a981
Patrick Yambrick <patrickyambrick@gmail.com>
2023-03-31 17:07:19 -0400

    Restyle skills page

--- pages/skills.js
+++ pages/skills.js
@@ -8,16 +8,17 @@ export default function Skills() {
         <section id="Skills" className={`${styles.skills} my-background-generic my-text`}>
             <div className="my-overlay">
                 <h3 className="text-center my-title padded-title">Skills</h3>
-                <ul className="grid rounded grid-cols-2 gap-2 p-6 text-center my-text md:mt-6 mt-2">
+                <ul className="rounded gap-4 p-6 text-center my-text md:mt-6 mt-2">
                     {skills.map((item) => {
                         return (
-                            <a className="col-span-1 p-2" href={item.url} key={item.name}>
-                                <li className={`my-text-forest-hover ${styles.listItem}`}>
+                            <li key={item.name} className={`my-text-forest-hover ${styles.listItem}`}>
+                                <a className="my-4" href={item.url}>
                                     {item.name}
-                                    <caption>
-                                        {item.description ? item.description : null}
-                                    </caption>
-                                </li></a>
+                                </a>
+                                <caption>
+                                    {item.description ? item.description : null}
+                                </caption>
+                            </li>
                         )
                     })}
 
