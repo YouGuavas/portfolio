8b8d5b1d0c51a42e37401e882fc3cfc9838f7b0d
Patrick Yambrick <patrickyambrick@gmail.com>
2023-06-15 19:38:58 -0400

    Fix linting errors

--- components/Lander.js
+++ components/Lander.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useCallback } from 'react'
 
 import { create, grabElement } from '../utils/_functions'
 
@@ -6,6 +6,17 @@ import styles from '../styles/Home.module.scss'
 import words from '../data/words.json'
 import letterWords from '../data/letterwords.json'
 
+//Render vertical divs for words
+const renderWords = letterWordsState => {
+    const parent = grabElement('titleWrapper')
+    letterWordsState.map((word, index) => {
+        if (grabElement(`${index}-outer`) !== null) return
+        const newDiv = create('div')
+        newDiv.id = `${index}-outer`
+        parent.append(newDiv)
+    })
+    return
+}
 export default function Lander() {
     const randomize = inputNumber => {
         return Math.floor(Math.random() * inputNumber)
@@ -13,14 +24,17 @@ export default function Lander() {
     const [letterWordsState, setLetterWordsState] = useState([])
 
     //Generate letterwords
-    const setLetters = (letters, outerIndex) => {
-        letters.map((letter, index) => {
-            const thisLetterWords = letterWords[0][letter.toUpperCase()]
-            const letterWord =
-                thisLetterWords[randomize(thisLetterWords.length)]
-            letterWordsState[outerIndex][index] = letterWord
-        })
-    }
+    const setLetters = useCallback(
+        (letters, outerIndex) => {
+            letters.map((letter, index) => {
+                const thisLetterWords = letterWords[0][letter.toUpperCase()]
+                const letterWord =
+                    thisLetterWords[randomize(thisLetterWords.length)]
+                letterWordsState[outerIndex][index] = letterWord
+            })
+        },
+        [letterWordsState]
+    )
 
     const addToParent = (parent, child) => {
         //Append child element to parent element
@@ -32,57 +46,52 @@ export default function Lander() {
             element.remove()
         }
     }
+    const resetWord = useCallback(
+        (outerIndex, index, word) => {
+            const letter = word.slice(0, 1)
+            //Search letterwords by first letter
+            const thisLetterWords = letterWords[0][letter.toUpperCase()]
+            const letterWord =
+                thisLetterWords[randomize(thisLetterWords.length)]
 
-    const addWordToCard = (word, outerIndex, index, parent) => {
-        //add new p element
-        const newP = create('p')
-        newP.id = `${outerIndex}-${index}-inner`
-
-        //Add new span for letterword and styling
-        const newSpan = create('span')
-        newSpan.innerHTML = word.slice(1)
-        newP.innerHTML = `${word.slice(0, 1)}`
-
-        //Reset the word each time the mouse moves away
-        newP.onclick = () => resetWord(outerIndex, index, word)
-        addToParent(newP, newSpan)
-        addToParent(parent, newP)
-    }
-
-    const resetWord = (outerIndex, index, word) => {
-        const letter = word.slice(0, 1)
-        //Search letterwords by first letter
-        const thisLetterWords = letterWords[0][letter.toUpperCase()]
-        const letterWord = thisLetterWords[randomize(thisLetterWords.length)]
-
-        const newWordsState = letterWordsState.map((outerItem, outerIdx) => {
-            const newArray = outerItem.map((item, innerIndex) => {
-                if (index === innerIndex) {
-                    return letterWord
-                } else {
-                    return item
+            const newWordsState = letterWordsState.map(
+                (outerItem, outerIdx) => {
+                    const newArray = outerItem.map((item, innerIndex) => {
+                        if (index === innerIndex) {
+                            return letterWord
+                        } else {
+                            return item
+                        }
+                    })
+                    if (outerIndex === outerIdx) return newArray
+                    return outerItem
                 }
-            })
-            if (outerIndex === outerIdx) return newArray
-            return outerItem
-        })
-        setLetterWordsState(newWordsState)
-    }
+            )
+            setLetterWordsState(newWordsState)
+        },
+        [letterWordsState]
+    )
+    const addWordToCard = useCallback(
+        (word, outerIndex, index, parent) => {
+            //add new p element
+            const newP = create('p')
+            newP.id = `${outerIndex}-${index}-inner`
 
-    //Render vertical divs for words
-    const renderWords = () => {
-        const parent = grabElement('titleWrapper')
-        letterWordsState.map((word, index) => {
-            if (grabElement(`${index}-outer`) !== null) return
-            const newDiv = create('div')
-            newDiv.id = `${index}-outer`
-            parent.append(newDiv)
-        })
-        return
-    }
+            //Add new span for letterword and styling
+            const newSpan = create('span')
+            newSpan.innerHTML = word.slice(1)
+            newP.innerHTML = `${word.slice(0, 1)}`
+
+            //Reset the word each time the mouse moves away
+            newP.onclick = () => resetWord(outerIndex, index, word)
+            addToParent(newP, newSpan)
+            addToParent(parent, newP)
+        },
+        [resetWord]
+    )
 
     //Render the Letters and letterwords
-    const renderLetters = () => {
+    const renderLetters = useCallback(() => {
         if (letterWordsState.length > 0) {
             letterWordsState.map((localLetterWords, outerIndex) => {
                 const parent = grabElement(`${outerIndex}-outer`) //document.getElementById(`${outerIndex}-outer`);
@@ -94,19 +103,19 @@ export default function Lander() {
                 })
             })
         }
-    }
+    }, [addWordToCard, letterWordsState])
     useEffect(() => {
         words.map((word, index) => {
             const letters = word.split('')
             letterWordsState[index] = []
             setLetters(letters, index)
-            renderWords()
+            renderWords(letterWordsState)
         })
         renderLetters()
-    }, [])
+    }, [renderLetters, setLetters, letterWordsState])
     useEffect(() => {
         renderLetters()
-    }, [letterWordsState])
+    }, [letterWordsState, renderLetters])
 
     return (
         <section id="Home" className={`${styles.lander}`}>
