fb4439594146eb884d726abcc55c253e417a32d3
Patrick <patrickyambrick@gmail.com>
2024-03-14 15:15:53 -0400

    Fix Word styling

--- components/Lander.js
+++ components/Lander.js
@@ -1,113 +1,111 @@
-import { useEffect, useState, useCallback } from 'react'
+import { useEffect, useState, useCallback } from 'react';
 
 import {
-    create,
-    grabElement,
-    addToParent,
-    clearElement,
-} from '../utils/_functions'
-import { renderWords } from '../utils/_wordFunctions'
+	create,
+	grabElement,
+	addToParent,
+	clearElement,
+} from '../utils/_functions';
+import { renderWords } from '../utils/_wordFunctions';
 
-import styles from '../styles/Home.module.scss'
-import words from '../data/words.json'
-import letterWords from '../data/letterwords.json'
+import styles from '../styles/Home.module.scss';
+import words from '../data/words.json';
+import letterWords from '../data/letterwords.json';
 
 export default function Lander() {
-    const randomize = inputNumber => {
-        return Math.floor(Math.random() * inputNumber)
-    }
-    const [letterWordsState, setLetterWordsState] = useState([])
-    //Generate letterwords
-    const setLetters = useCallback(
-        (letters, outerIndex) => {
-            letters.map((letter, index) => {
-                const thisLetterWords = letterWords[0][letter.toUpperCase()]
-                const letterWord =
-                    thisLetterWords[randomize(thisLetterWords.length)]
-                letterWordsState[outerIndex][index] = letterWord
-            })
-        },
-        [letterWordsState]
-    )
+	const randomize = (inputNumber) => {
+		return Math.floor(Math.random() * inputNumber);
+	};
+	const [letterWordsState, setLetterWordsState] = useState([]);
+	//Generate letterwords
+	const setLetters = useCallback(
+		(letters, outerIndex) => {
+			letters.map((letter, index) => {
+				const thisLetterWords = letterWords[0][letter.toUpperCase()];
+				const letterWord = thisLetterWords[randomize(thisLetterWords.length)];
+				letterWordsState[outerIndex][index] = letterWord;
+			});
+		},
+		[letterWordsState]
+	);
 
-    const resetWord = (outerIndex, index, word) => {
-        const letter = word.slice(0, 1)
-        //Search letterwords by first letter
-        const thisLetterWords = letterWords[0][letter.toUpperCase()]
-        const letterWord = thisLetterWords[randomize(thisLetterWords.length)]
-        const newWordsState = []
-        letterWordsState.map((outerItem, outerIdx) => {
-            const newArray = outerItem.map((item, innerIndex) => {
-                if (index === innerIndex) {
-                    return letterWord
-                } else {
-                    return item
-                }
-            })
-            if (outerIndex === outerIdx) {
-                newWordsState[outerIndex] = newArray
-            }
-            return outerItem
-        })
-        setLetterWordsState(newWordsState)
-    }
-    const addWordToCard = (word, outerIndex, index, parent) => {
-        //add new p element
-        const newP = create('p')
-        newP.id = `${outerIndex}-${index}-inner`
+	const resetWord = (outerIndex, index, word) => {
+		const letter = word.slice(0, 1);
+		//Search letterwords by first letter
+		const thisLetterWords = letterWords[0][letter.toUpperCase()];
+		const letterWord = thisLetterWords[randomize(thisLetterWords.length)];
+		const newWordsState = [];
+		letterWordsState.map((outerItem, outerIdx) => {
+			const newArray = outerItem.map((item, innerIndex) => {
+				if (index === innerIndex) {
+					return letterWord;
+				} else {
+					return item;
+				}
+			});
+			if (outerIndex === outerIdx) {
+				newWordsState[outerIndex] = newArray;
+			}
+			return outerItem;
+		});
+		setLetterWordsState(newWordsState);
+	};
+	const addWordToCard = (word, outerIndex, index, parent) => {
+		//add new p element
+		const newP = create('p');
+		newP.id = `${outerIndex}-${index}-inner`;
 
-        //Add new span for letterword and styling
-        const newSpan = create('span')
-        newSpan.innerHTML = word.slice(1)
-        newP.innerHTML = `${word.slice(0, 1)}`
+		//Add new span for letterword and styling
+		const newSpan = create('span');
+		newSpan.innerHTML = word.slice(1);
+		newP.innerHTML = `${word.slice(0, 1)}`;
 
-        //Reset the word each time the mouse moves away
-        newP.onclick = () => resetWord(outerIndex, index, word)
-        addToParent(newP, newSpan)
-        addToParent(parent, newP)
-    }
+		//Reset the word each time the mouse moves away
+		newP.onclick = () => resetWord(outerIndex, index, word);
+		addToParent(newP, newSpan);
+		addToParent(parent, newP);
+	};
 
-    //Render the Letters and letterwords
-    const renderLetters = () => {
-        if (letterWordsState.length > 0) {
-            letterWordsState.map((localLetterWords, outerIndex) => {
-                const parent = grabElement(`${outerIndex}-outer`) //document.getElementById(`${outerIndex}-outer`);
+	//Render the Letters and letterwords
+	const renderLetters = () => {
+		if (letterWordsState.length > 0) {
+			letterWordsState.map((localLetterWords, outerIndex) => {
+				const parent = grabElement(`${outerIndex}-outer`);
 
-                localLetterWords.map((letterWord, index) => {
-                    const element = grabElement(`${outerIndex}-${index}-inner`)
-                    clearElement(element)
-                    addWordToCard(letterWord, outerIndex, index, parent)
-                })
-            })
-        }
-    }
-    useEffect(() => {
-        words.map((word, index) => {
-            const letters = word.split('')
-            letterWordsState[index] = []
-            setLetters(letters, index)
-            renderWords(letterWordsState)
-        })
-        renderLetters()
-    }, [])
-    useEffect(() => {
-        renderLetters()
-    }, [letterWordsState])
+				localLetterWords.map((letterWord, index) => {
+					const element = grabElement(`${outerIndex}-${index}-inner`);
+					clearElement(element);
+					addWordToCard(letterWord, outerIndex, index, parent);
+				});
+			});
+		}
+	};
+	useEffect(() => {
+		words.map((word, index) => {
+			const letters = word.split('');
+			letterWordsState[index] = [];
+			setLetters(letters, index);
+			renderWords(letterWordsState);
+		});
+		renderLetters();
+	}, []);
+	useEffect(() => {
+		renderLetters();
+	}, [letterWordsState]);
 
-    return (
-        <section id="Home" className={`${styles.lander}`}>
-            <div className={styles.landerOverlay}></div>
-            <div
-                id="titleWrapper"
-                className={`my-rotate main-wrapper ${styles.titleWrapper}`}
-            >
-                <div className={styles.flexTitle}>
-                    <h1 className={styles.banner}>
-                        Hi, I&apos;m Patrick Yambrick
-                    </h1>
-                    <p>Try tapping or clicking on a word!</p>
-                </div>
-            </div>
-        </section>
-    )
+	return (
+		<section id="Home" className={`${styles.lander}`}>
+			<div className={styles.landerOverlay}></div>
+			<div
+				id="titleWrapper"
+				className={`my-rotate main-wrapper ${styles.titleWrapper}`}
+			>
+				<div className={styles.flexTitle}>
+					<h1 className={styles.banner}>Hi, I&apos;m Patrick Yambrick</h1>
+					<p>Try tapping or clicking on a word!</p>
+				</div>
+				<div id="wordWrapper" className={`${styles.wordWrapper}`}></div>
+			</div>
+		</section>
+	);
 }
--- utils/_wordFunctions.js
+++ utils/_wordFunctions.js
@@ -1,15 +1,15 @@
-import { create, grabElement } from './_functions'
+import { create, grabElement } from './_functions';
 
 //Render vertical divs for words
-const renderWords = letterWordsState => {
-    const parent = grabElement('titleWrapper')
-    letterWordsState.map((word, index) => {
-        if (grabElement(`${index}-outer`) !== null) return
-        const newDiv = create('div')
-        newDiv.id = `${index}-outer`
-        parent.append(newDiv)
-    })
-    return
-}
+const renderWords = (letterWordsState) => {
+	const parent = grabElement('wordWrapper');
+	letterWordsState.map((word, index) => {
+		if (grabElement(`${index}-outer`) !== null) return;
+		const newDiv = create('div');
+		newDiv.id = `${index}-outer`;
+		parent.append(newDiv);
+	});
+	return;
+};
 
-export { renderWords }
+export { renderWords };
